03cde6a3c1265d79c58fbac970ca6a8bb4479825
affjljoo3581 <affjljoo3581@gmail.com>
2020-09-05 14:34:48 +0900

    Implement a builder for merging files

---
+++ src/langumo/building/mergence.py
@@ -0,0 +1,21 @@
+from langumo.building import Builder
+from langumo.utils import AuxiliaryFile, AuxiliaryFileManager, colorful
+
+
+class MergeFiles(Builder):
+    def build(self, afm: AuxiliaryFileManager, *inputs: AuxiliaryFile
+              ) -> AuxiliaryFile:
+        merged = afm.create()
+
+        print(colorful.render(f'<r>[*]</r> merge <g>{len(inputs)}</g> files'))
+        with merged.open('wb') as dst, \
+                AuxiliaryFile.opens(inputs, 'rb') as srcs:
+            for src in srcs:
+                for line in src:
+                    # Add break-line character to the end of text to avoid
+                    # being merged with other line.
+                    line += b'\n' if not line.endswith(b'\n') else b''
+
+                    dst.write(line)
+
+        return merged

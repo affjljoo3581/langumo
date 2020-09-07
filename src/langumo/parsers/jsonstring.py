9120208164964a918475348a0233e4eab21c3bd1
affjljoo3581 <affjljoo3581@gmail.com>
2020-09-07 22:00:08 +0900

    Add to normalize the content

--- src/langumo/parsers/jsonstring.py
+++ src/langumo/parsers/jsonstring.py
@@ -1,3 +1,4 @@
+import re
 import json
 from langumo.building import Parser
 from langumo.utils import AuxiliaryFile
@@ -5,6 +6,9 @@ from typing import Iterable
 
 
 class EscapedJSONStringParser(Parser):
+    single_quotes_pattern = re.compile('[\x60\xb4\u2018\u2019]')
+    double_quotes_pattern = re.compile('[\u201c\u201d]')
+
     def extract(self, raw: AuxiliaryFile) -> Iterable[str]:
         with raw.open('r') as fp:
             for line in fp:
@@ -15,4 +19,23 @@ class EscapedJSONStringParser(Parser):
 
     def parse(self, text: str) -> str:
         text, _ = json.decoder.scanstring(text, 1)
-        return text
+
+        filtered = []
+        for line in text.strip().splitlines():
+            if not line:
+                continue
+
+            line = line.replace('\n', ' ').replace('\t', ' ')
+            while '  ' in line:
+                line = line.replace('  ', ' ')
+
+            # Normalize the quotes by replacing unusual ones to the standard
+            # ones.
+            line = (EscapedJSONStringParser
+                    .single_quotes_pattern.sub('\'', line))
+            line = (EscapedJSONStringParser
+                    .double_quotes_pattern.sub('"', line))
+
+            filtered.append(line)
+
+        return '\n'.join(filtered)

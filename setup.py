7a9eb6ac8f54ce648ab870eb18c0ba0edd04bcd2
affjljoo3581 <affjljoo3581@gmail.com>
2020-09-27 23:29:05 +0900

    Fix dependency

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
         'colorama',
         'pyyaml>=5.3.1',
         'tqdm>=4.46.0',
-        'tokenizers>=0.8.0',
+        'tokenizers==0.8.1',
         'mwparserfromhell>=0.5.4',
         'kss==1.3.1'
     ],
